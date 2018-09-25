b6f3e9314095bff464c9999091f9336f951eceb3
Adailson Santos <adailson2@gmail.com>
2018-09-25 00:25:17 -0300

    Pedido, EstadoPagamento and Pagamento

--- src/main/java/com/adailsonsantos/cursomc/CursomcApplication.java
+++ src/main/java/com/adailsonsantos/cursomc/CursomcApplication.java
@@ -1,25 +1,17 @@
 package com.adailsonsantos.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
+import com.adailsonsantos.cursomc.domain.*;
+import com.adailsonsantos.cursomc.domain.enums.EstadoPagamento;
+import com.adailsonsantos.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.adailsonsantos.cursomc.domain.Categoria;
-import com.adailsonsantos.cursomc.domain.Cidade;
-import com.adailsonsantos.cursomc.domain.Cliente;
-import com.adailsonsantos.cursomc.domain.Endereco;
-import com.adailsonsantos.cursomc.domain.Estado;
-import com.adailsonsantos.cursomc.domain.Produto;
 import com.adailsonsantos.cursomc.domain.enums.TipoCliente;
-import com.adailsonsantos.cursomc.repositories.CategoriaRepository;
-import com.adailsonsantos.cursomc.repositories.CidadeRepository;
-import com.adailsonsantos.cursomc.repositories.ClienteRepository;
-import com.adailsonsantos.cursomc.repositories.EnderecoRepository;
-import com.adailsonsantos.cursomc.repositories.EstadoRepository;
-import com.adailsonsantos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -36,6 +28,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,6 +80,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+        Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+
+        cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+        pedidoRepository.save(Arrays.asList(ped1, ped2));
+        pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
 	}
 	
 }
