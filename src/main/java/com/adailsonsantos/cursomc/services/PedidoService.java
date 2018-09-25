77624276489e4bd585282c9a73621729279d539b
Adailson Santos <adailson2@gmail.com>
2018-09-25 19:30:50 -0300

    Endpoint /pedidos/{id} available

---
+++ src/main/java/com/adailsonsantos/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.adailsonsantos.cursomc.services;
+
+import com.adailsonsantos.cursomc.domain.Pedido;
+import com.adailsonsantos.cursomc.repositories.PedidoRepository;
+import com.adailsonsantos.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
