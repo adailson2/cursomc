d53493a6811c48b2fc8ff6edb1e6ec49d6f9ed7f
Adailson Santos <adailson2@gmail.com>
2018-09-29 14:26:41 -0300

    Add develpment profile

--- src/main/java/com/adailsonsantos/cursomc/CursomcApplication.java
+++ src/main/java/com/adailsonsantos/cursomc/CursomcApplication.java
@@ -1,17 +1,8 @@
 package com.adailsonsantos.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import com.adailsonsantos.cursomc.domain.*;
-import com.adailsonsantos.cursomc.domain.enums.EstadoPagamento;
-import com.adailsonsantos.cursomc.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.adailsonsantos.cursomc.domain.enums.TipoCliente;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
