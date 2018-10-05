f2ba192d3b8d15edcc40fff6aaceeaa3c81ae94b
Adailson Santos <adailson2@gmail.com>
2018-10-05 00:04:05 -0300

    Email HTML

--- src/main/java/com/adailsonsantos/cursomc/services/EmailService.java
+++ src/main/java/com/adailsonsantos/cursomc/services/EmailService.java
@@ -3,9 +3,15 @@ package com.adailsonsantos.cursomc.services;
 import com.adailsonsantos.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
